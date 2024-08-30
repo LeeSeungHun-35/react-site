72b4c7aa766947ef41ba157e191a6b1917741f4a
LeeSeungHun-35 <ish0542@naver.com>
2024-08-30 11:58:38 +0900

    수정

--- src/pages/Symptom.jsx
+++ src/pages/Symptom.jsx
@@ -4,6 +4,7 @@ import './Symptom.css';
 const Symptom = () => {
     const [selectedSymptoms, setSelectedSymptoms] = useState([]);
 
+    /* 체크박스를 아무거나 선택하면 그 값을 목록에 추가하고나 제거시킴. */
     const handleCheckboxChange = (event) => {
         const value = event.target.value;
         setSelectedSymptoms((prevSelected) =>
@@ -13,31 +14,32 @@ const Symptom = () => {
         );
     };
 
-    const symptoms = [      { value: 'fever', label: '열' },
-                            { value: 'cough', label: '콧물' },
-                            { value: 'headache', label: '두통' },
-                            { value: 'fatigue', label: '피로' },
-                            { value: 'nausea', label: '메스꺼움' },
-                            { value: 'chills', label: '오한' }
-                     ];
+    const symptoms = [{ value: 'fever', label: '열' },
+    { value: 'cough', label: '콧물' },
+    { value: 'headache', label: '두통' },
+    { value: 'fatigue', label: '피로' },
+    { value: 'nausea', label: '메스꺼움' },
+    { value: 'chills', label: '오한' }
+    ];
 
-    const diseaseMapping =  {
-                            fever: ['감기', '독감'],
-                            cough: ['감기', '기관지염'],
-                            headache: ['편두통', '긴장성 두통'],
-                            fatigue: ['빈혈', '만성 피로 증후군'],
-                            nausea: ['위염', '위장 장애'],
-                            chills: ['독감', '감기']
-                            };
+    const diseaseMapping = {
+        fever: ['감기', '독감'],
+        cough: ['감기', '기관지염'],
+        headache: ['편두통', '긴장성 두통'],
+        fatigue: ['빈혈', '만성 피로 증후군'],
+        nausea: ['위염', '위장 장애'],
+        chills: ['독감', '감기']
+    };
 
     const relatedDiseases = Array.from(
-        new Set(
+        new Set( /* new set=중복은 하나로 처리 (제거) */
             selectedSymptoms.flatMap(symptom => diseaseMapping[symptom] || [])
         )
     );
 
     return (
         <div className='symptom'>
+            {/* 증상 선택하면 */}
             <div className='sym1'>
                 <h2>증상 기반 (일상)</h2>
                 <form>
@@ -58,14 +60,15 @@ const Symptom = () => {
             </div>
 
             <div className='disease-list'>
-                <h3>관련 병명</h3>
+                {/* 병명 출력해준ㄴ다 */}
+                <h2>관련 병명</h2>
                 <ul>
                     {relatedDiseases.length > 0 ? (
                         relatedDiseases.map((disease, index) => (
                             <li key={index}>{disease}</li>
                         ))
                     ) : (
-                        <li className='empty'>선택된 증상에 대한 병명이 없습니다.</li>
+                        <li className='empty'>현재 가지고있는 증상을 체크해주세요</li>
                     )}
                 </ul>
             </div>
